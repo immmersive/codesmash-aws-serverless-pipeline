b2e57387884b68239c1b92685336b3a1744f6e19
Immersive <105320165+immmersive@users.noreply.github.com>
2023-09-12 22:33:21 +0900

    Add files via upload

---
+++ inputs.tf
@@ -0,0 +1,19 @@
+variable "cicd_name" {    
+    default = ""
+}
+
+variable "repo_name" {
+    default = ""
+}
+
+variable "repo_branch" {
+    default = ""
+}
+ 
+variable "region" {
+  default = ""
+}
+
+variable "bucket" {
+  default = ""
+}
